b4e9e8b363d1546979f1b6b6e5af6d605baaefde
jPiconi <juliaap.piconi@gmail.com>
2025-03-14 15:31:12 -0300

    ultimo commit do dia 14/03

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const express = require("express");
+const express = require("express"); // importa lib do Express
 
-const app = express();
+const sqlite3 = require("sqlite3"); // Importa lib do sqlite3
 
 const PORT = 8000; //porta TCP do servidor HHTP da aplicação;
 
-const index =
-  "<a href='/sobre'> Sobre </a> <a href='/information'> Informações  </a>";
+const app = express(); //Instância para uso do Express
 
-const sobre = 'Você está na pagina "Sobre"<br><a href="/">Voltar</a>';
-const information =
-  'Você está na pagina "Information"<br><a href="/">Voltar</a>';
+const db = new sqlite3.Database("user.db"); //Instância para uso do Sqlite3, e usa o arquivo 'user.db'
+
+db.serialize(() => {
+  //Este método permite enviar comandos SQL em modo 'sequencial'
+  db.run(
+    "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT )"
+  );
+});
+
+//Cria conexão com o banco de dados
+
+const index =
+  "<a href='/Home'> Home </a> <a href='/Sobre'> Sobre </a> <a href='/Login'> Login </a> <a href='/Cadastro'> Cadastro </a>";
+
+const Home = 'Você está na pagina "Home"<br><a href="/">Voltar</a>';
+const Sobre = 'Você está na pagina "Sobre"<br><a href="/">Voltar</a>';
+const Login = 'Você está na pagina de "Login"<br><a href="/">Voltar</a>';
+const Cadastro = 'Você está na pagina de "Cadastro"<br><a href="/">Voltar</a>';
 
 /*Método express.get necessita de dois parâmetros 
 Na ARROW FUNCTION, o primeiro são os dados de servidor (REQUISITION - 'req')
@@ -19,12 +33,20 @@ app.get("/", (req, res) => {
   res.send(index);
 });
 
-app.get("/sobre", (req, res) => {
-  res.send(sobre);
+app.get("/Home", (req, res) => {
+  res.send(Home);
 });
 
-app.get("/information", (req, res) => {
-  res.send(information);
+app.get("/Sobre", (req, res) => {
+  res.send(Sobre);
+});
+
+app.get("/Login", (req, res) => {
+  res.send(Login);
+});
+
+app.get("/Cadastro", (req, res) => {
+  res.send(Cadastro);
 });
 
 //app listen deve ser o último comando da aplicação (app.js);
