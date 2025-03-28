8b3c05ffed8f537152bbc7b344e744f0171e91bf
jPiconi <juliaap.piconi@gmail.com>
2025-03-28 07:47:11 -0300

    commits do dia 28/03

--- app.js
+++ app.js
@@ -15,39 +15,50 @@ db.serialize(() => {
   );
 });
 
+app.use("/static", express.static(__dirname + "/static"));
+
+//Configurar EJS como o motor da visualização
+app.set("view engine", "ejs");
+
 //Cria conexão com o banco de dados
 
-const index =
-  "<a href='/Home'> Home </a> <a href='/Sobre'> Sobre </a> <a href='/Login'> Login </a> <a href='/Cadastro'> Cadastro </a>";
+// const index =
+//   "<a href='/Home'> Home </a> <a href='/Sobre'> Sobre </a> <a href='/Login'> Login </a> <a href='/Cadastro'> Cadastro </a>";
 
-const Home = 'Você está na pagina "Home"<br><a href="/">Voltar</a>';
-const Sobre = 'Você está na pagina "Sobre"<br><a href="/">Voltar</a>';
-const Login = 'Você está na pagina de "Login"<br><a href="/">Voltar</a>';
-const Cadastro = 'Você está na pagina de "Cadastro"<br><a href="/">Voltar</a>';
+// const Home = 'Você está na pagina "Home"<br><a href="/">Voltar</a>';
+// const Sobre = 'Você está na pagina "Sobre"<br><a href="/">Voltar</a>';
+// const Login = 'Você está na pagina de "Login"<br><a href="/">Voltar</a>';
+// const Cadastro = 'Você está na pagina de "Cadastro"<br><a href="/">Voltar</a>';
 
 /*Método express.get necessita de dois parâmetros 
 Na ARROW FUNCTION, o primeiro são os dados de servidor (REQUISITION - 'req')
 o segundo, são os dados que serão enviados ao cliente (RESULT - 'res')*/
 
 app.get("/", (req, res) => {
-  res.send(index);
+  //Rota raiz do meu servidor, acesse o browser com o endereço http://localhost:8000/
+  // res.send(index);
+  res.render("index");
 });
 
-app.get("/Home", (req, res) => {
-  res.send(Home);
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.get("/Sobre", (req, res) => {
-  res.send(Sobre);
+app.post("/login", (req, res) => {
+  res.send("Login ainda não efetuado");
 });
+//programação de rotas do método GET do HTTP
+// app.get("/Home", (req, res) => {
+//   res.send(Home);
+// });
 
-app.get("/Login", (req, res) => {
-  res.send(Login);
-});
+// app.get("/Sobre", (req, res) => {
+//   res.send(Sobre);
+// });
 
-app.get("/Cadastro", (req, res) => {
-  res.send(Cadastro);
-});
+// app.get("/Cadastro", (req, res) => {
+//   res.send(Cadastro);
+// });
 
 //app listen deve ser o último comando da aplicação (app.js);
 app.listen(PORT, () => {
